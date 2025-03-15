a21a3303ce67572e8f642fb3a2ddfab13efe40b6
Onur Atabay <onuratabay@hotmail.com>
2025-03-15 13:57:32 +0300

    yeni commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { OrderProvider } from './contexts/OrderContext';
+import { UserProvider, UserRoles } from './contexts/UserContext';
+import theme from './styles/theme';
+
+import TableView from './components/TableView';
+import QRScanner from './components/QRScanner';
+import PaymentScreen from './components/PaymentScreen';
+import MenuView from './components/MenuView';
+import LoginScreen from './components/LoginScreen';
+import AdminPanel from './components/AdminPanel';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <UserProvider>
+        <OrderProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<QRScanner />} />
+              <Route path="/table/:tableId" element={<TableView />} />
+              <Route path="/payment/:tableId" element={<PaymentScreen />} />
+              <Route path="/menu/:tableId" element={<MenuView />} />
+              <Route path="/bill/:tableId" element={<PaymentScreen />} />
+              <Route path="/login" element={<LoginScreen />} />
+              <Route
+                path="/admin/*"
+                element={
+                  <ProtectedRoute roles={[UserRoles.ADMIN, UserRoles.WAITER]}>
+                    <AdminPanel />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </Router>
+        </OrderProvider>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
